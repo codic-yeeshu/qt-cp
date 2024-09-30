0993e4a7b8c171eab2d43a5afcc493c7aae014c4
codic-yeeshu <yeeshu.exploresmore@gmail.com>
2024-09-30 17:35:54 +0530

    initial commit

---
+++ Atcoder/practice-sheets/educational-dp-contest/A-frog-1.py
@@ -0,0 +1,24 @@
+'''
+Problem Statement : https://atcoder.jp/contests/dp/tasks/dp_a
+'''
+
+import sys
+sys.setrecursionlimit(10**6)
+
+def minPrice(start, end, prices, memo):
+    if start == end:
+        return 0
+    if (start, end) in memo:
+        return memo[(start, end)]
+
+    next_cost = abs(prices[start] - prices[start+1]) + minPrice(start+1, end, prices, memo)
+    skip_cost = float('inf')
+    if start + 2 <= end:
+        skip_cost = abs(prices[start] - prices[start+2]) + minPrice(start+2, end, prices, memo)
+    
+   
+    memo[(start, end)] = min(next_cost, skip_cost)
+    return memo[(start, end)]
+
+n = int(input())
+prices = list(map(int, input().split()))
